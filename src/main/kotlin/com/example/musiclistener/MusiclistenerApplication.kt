4dabde13264f25d8559765861cf99300e1d777df
Jyoti Jauhari <jyoti.jauhari@thoughtworks.com>
2022-11-04 12:06:35 +0530

    Add. initial commit

---
+++ src/main/kotlin/com/example/musiclistener/MusiclistenerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.musiclistener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusiclistenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusiclistenerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/musiclistener/MusiclistenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.musiclistener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusiclistenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
